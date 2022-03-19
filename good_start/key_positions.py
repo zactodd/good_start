0955dcd56c381fc72552fc41679c11ded7b3d119
Zac Todd <zactodd0@gmail.com>
2022-03-19 13:15:19 +1300

    Create key_positions.py

---
+++ good_start/key_positions.py
@@ -0,0 +1,27 @@
+
+# Buttons
+# Menu buttons
+PLAY_BUTTON = ()
+CUSTOM_GAME_BUTTON = ()
+ORDER_BUTTON = ()
+
+
+NEXT_BUTTON = ()
+
+TURN_START_BUTTON = ()
+
+# Resource Buttons
+FRUIT_BUTTON = ()
+FISH_BUTTON = ()
+INVERTEBRATE_BUTTON = ()
+RODENT_BUTTON = ()
+SEED_BUTTON = ()
+
+# Bird card positions
+
+
+# Bonus card positions
+
+
+# Menus order
+MENU_TRAVERSAL = (PLAY_BUTTON, CUSTOM_GAME_BUTTON, ORDER_BUTTON, NEXT_BUTTON)

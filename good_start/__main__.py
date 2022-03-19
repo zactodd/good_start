7fdddf44b0fb271d1d916d1e262908ba5eee19c4
Zac Todd <zactodd0@gmail.com>
2022-03-19 19:59:50 +1300

    Update __main__.py

--- good_start/__main__.py
+++ good_start/__main__.py
@@ -5,14 +5,13 @@ from read_cards import *
 
 
 if __name__ == '__main__':
-    time.sleep(5)
+    time.sleep(3)
     print('Starting...')
     menu_from_start()
-
     while True:
         time.sleep(6)
         move_and_click(*key_positions.TURN_START_BUTTON)
-        time.sleep(3)
+        time.sleep(2)
 
         birds, centres = extract_bird_cards()
         bird_centres = dict(zip(birds, centres))
@@ -22,13 +21,14 @@ if __name__ == '__main__':
             birds, food = selection
             print('Selected birds:', birds)
             print('Selected food:', food)
-
             for b in birds:
                 move_and_click(*bird_centres[b])
                 time.sleep(0.5)
             for f in food:
                 move_and_click(*key_positions.RESOURCES_TO_BUTTONS[f])
                 time.sleep(0.5)
+            move_and_click(*key_positions.NEXT_BUTTON)
+            time.sleep(1)
 
             bonuses, centres = extract_bonus_cards()
             bonus_centres = dict(zip(bonuses, centres))
@@ -36,7 +36,7 @@ if __name__ == '__main__':
             print('Selected bonus:', bonus)
             print()
 
-            move_and_click(bonus_centres[bonus])
+            move_and_click(*bonus_centres[bonus])
             time.sleep(0.5)
             move_and_click(*key_positions.NEXT_BUTTON)
             time.sleep(30)

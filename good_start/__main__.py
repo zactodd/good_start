7fa507441cbf0d49f72c5577d8be8f4dce237c20
Zac Todd <zactodd0@gmail.com>
2022-03-19 19:17:46 +1300

    Create __main__.py

---
+++ good_start/__main__.py
@@ -0,0 +1,48 @@
+from card_selection import *
+from gui_interactions import *
+from read_cards import *
+
+
+
+if __name__ == '__main__':
+    time.sleep(5)
+    print('Starting...')
+    menu_from_start()
+
+    while True:
+        time.sleep(6)
+        move_and_click(*key_positions.TURN_START_BUTTON)
+        time.sleep(3)
+
+        birds, centres = extract_bird_cards()
+        bird_centres = dict(zip(birds, centres))
+
+        selection = bird_selection(birds)
+        if selection:
+            birds, food = selection
+            print('Selected birds:', birds)
+            print('Selected food:', food)
+
+            for b in birds:
+                move_and_click(*bird_centres[b])
+                time.sleep(0.5)
+            for f in food:
+                move_and_click(*key_positions.RESOURCES_TO_BUTTONS[f])
+                time.sleep(0.5)
+
+            bonuses, centres = extract_bonus_cards()
+            bonus_centres = dict(zip(bonuses, centres))
+            bonus = bonus_selection(bonuses)
+            print('Selected bonus:', bonus)
+            print()
+
+            move_and_click(bonus_centres[bonus])
+            time.sleep(0.5)
+            move_and_click(*key_positions.NEXT_BUTTON)
+            time.sleep(30)
+        new_game_from_game()
+
+
+
+
+

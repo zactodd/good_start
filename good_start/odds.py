6e2a487621bdfe0dcfc73e7d4cd02a41b4ef0db5
Zac Todd <zactodd0@gmail.com>
2022-04-06 16:19:21 +1200

    Update odds.py

--- good_start/odds.py
+++ good_start/odds.py
@@ -34,14 +34,17 @@ def good_game_selection_with_tray(sample=1000):
     plt.show()
 
 
-def good_game_selection_with_double_tray(sample=1000):
+def good_game_selection_with_tray_and_humingbird(sample=1000):
     good_games = Counter()
     for cards in map(lambda _: random.sample(BIRD_NAMES, 8), range(sample)):
         hand = cards[:5]
         tray = cards[5:]
         for bird in BIRD_IMPORTANCE:
             required_birds = bird['birds']
-            if all(b in hand for b in required_birds) and len({BIRD_GROUPS[b ]for b in bird['tray'] if b in tray}) > 1:
+            required_tray = bird['tray']
+            if all(b in hand for b in required_birds) and \
+                    any(b in BIRD_GROUPS and 'Hummingbird' in BIRD_GROUPS[b] for b in tray) and \
+                any(b in required_tray and not (b in BIRD_GROUPS and 'Hummingbird' in BIRD_GROUPS[b]) for b in tray):
                 name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in required_birds)
                 good_games[name] += 1
     good_games = {k: v / sample for k, v in good_games.items()}
@@ -51,7 +54,7 @@ def good_game_selection_with_double_tray(sample=1000):
 
 
 n = 1000000
-good_game_selection(n)
+# good_game_selection(n)
 good_game_selection_with_tray(n)
-good_game_selection_with_double_tray(n)
+good_game_selection_with_tray_and_humingbird(n)
 

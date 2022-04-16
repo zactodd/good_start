c2323bb4370408f66bd7a1623a2fa48233e66ed5
Zac Todd <zactodd0@gmail.com>
2022-04-17 09:28:34 +1200

    Update gui_interactions.py

--- good_start/gui_interactions.py
+++ good_start/gui_interactions.py
@@ -4,22 +4,33 @@ import time
 import ctypes
 
 
-def _window_coords_from_name(name: str)-> tuple:
+def _window_bbox_from_name(name: str)-> tuple:
     hwnd = ctypes.windll.user32.FindWindowW(0, name)
     rect = ctypes.wintypes.RECT()
     ctypes.windll.user32.GetWindowRect(hwnd, ctypes.pointer(rect))
     x0, y0, x1, y1 = rect.left, rect.top, rect.right, rect.bottom
-    return x0, y0, x1 - x0, y1 - y0
+    return x0, y0, x1, y1
 
 
-WINDOW = WINDOW_X0, WINDOW_Y0, WINDOW_W, WINDOW_H = _window_coords_from_name('Wingspan')
-WINDOW_X1, WINDOW_Y1 = WINDOW_X0 + WINDOW_W, WINDOW_Y0 + WINDOW_H
+def window_bbox() -> tuple:
+    return _window_bbox_from_name('Wingspan')
+
+
+def window_dimensions() -> tuple:
+    x0, y0, x1, y1 = window_bbox()
+    return x1 - x0, y1 - y0
 
 
 def move_and_click(x: float, y: float) -> None:
+    w, h = window_dimensions()
+    x0, y0, *_ = window_bbox()
     if x > 1:
-        x, y = x / WINDOW_W, y / WINDOW_H
-    autoit.mouse_click("left", round(WINDOW_X0 + x * WINDOW_W), round(WINDOW_Y0 + y * WINDOW_H), 1, 1)
+        x, y = x / w, y / h
+    autoit.mouse_click("left", round(x0 + x * w), round(y0 + y * h), 1, 1)
+
+
+def activate_window():
+    autoit.win_activate('Wingspan')
 
 
 def click_buttons(buttons, wait=0.5) -> None:
@@ -38,4 +49,3 @@ def new_game_from_game() -> None:
 
 def exit_game() -> None:
     click_buttons(key_positions.EXIT_GAME)
-

c60e37b0052c82024fa36af3f95918e808e8cb90
Zac Todd <zactodd0@gmail.com>
2022-04-18 10:23:41 +1200

    Update card_selection.py

--- good_start/card_selection.py
+++ good_start/card_selection.py
@@ -4,11 +4,11 @@ import utils
 def bird_selection(hand, tray):
     for items in utils.BIRD_IMPORTANCE:
         birds, food, tray_req = items.values()
-        if all(b in hand for b in birds) and any(b in tray for b in tray_req):
+        if all(b in hand for b in birds) and any(b in tray_req for b in tray):
             if len(birds) + len(food) == 5:
-                return birds, food, tray
+                return birds, food
             else:
-                return birds + [next(c for c in hand if c not in birds)], food, tray
+                return birds + [next(c for c in hand if c not in birds)], food
 
 
 def bonus_selection(cards):

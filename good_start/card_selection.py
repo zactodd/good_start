a02c3e7a519721bd67d4aece9b2dfbaf39b29140
Zac Todd <zactodd0@gmail.com>
2022-04-23 13:43:04 +1200

    Update card_selection.py

--- good_start/card_selection.py
+++ good_start/card_selection.py
@@ -1,7 +1,8 @@
 import utils
+from typing import List, Tuple
 
 
-def bird_selection(hand, tray):
+def bird_selection(hand: List[str], tray: List[str]) -> Tuple[List[str], List[str]]:
     for items in utils.BIRD_IMPORTANCE:
         birds, food, tray_req = items.values()
         if all(b in hand for b in birds) and any(b in tray_req for b in tray):
@@ -11,5 +12,5 @@ def bird_selection(hand, tray):
                 return birds + [next(c for c in hand if c not in birds)], food
 
 
-def bonus_selection(cards):
+def bonus_selection(cards: List[str]) -> str:
     return min(cards, key=lambda c: utils.BONUS_IMPORTANCE.index(c))

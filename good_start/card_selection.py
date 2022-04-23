17927692682698784b16cc06148f99fac4736479
Zac Todd <zactodd0@gmail.com>
2022-04-23 19:59:53 +1200

    Update card_selection.py

--- good_start/card_selection.py
+++ good_start/card_selection.py
@@ -1,15 +1,26 @@
 import utils
-from typing import List, Tuple
+from typing import Set, Tuple, List
+import random
 
 
-def bird_selection(hand: List[str], tray: List[str]) -> Tuple[List[str], List[str]]:
+def bird_selection(hand: List[str], tray: List[str]) -> Tuple[List[str], Set[str]]:
     for items in utils.BIRD_IMPORTANCE:
-        birds, food, tray_req = items.values()
+        birds, food_suggestion, tray_req = items.values()
         if all(b in hand for b in birds) and any(b in tray_req for b in tray):
-            if len(birds) + len(food) == 5:
-                return birds, food
+            tray_wants = [b for b in tray if b in tray_req]
+            if '*' in birds:
+                return birds + [next(c for c in hand if c not in birds)], food_suggestion
             else:
-                return birds + [next(c for c in hand if c not in birds)], food
+                food_count = 5 - len(birds)
+                food = {f for b in birds + tray_wants for _, fs in utils.BIRD_FOOD[b] for f in fs.keys()}
+                food -= {'Wild'}
+                if len(food) == food_count:
+                    return birds, food
+                elif len(food) < food_count:
+                    return birds, food | set(random.sample(set(utils.FOOD) - food, food_count - len(food)))
+                else:
+                    return birds, food_suggestion
+
 
 
 def bonus_selection(cards: List[str]) -> str:

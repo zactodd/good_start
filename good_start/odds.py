bb271a863dfb4cc61cce3eb94480f2e7deb25749
Zac Todd <zactodd0@gmail.com>
2022-05-02 10:32:34 +1200

    Update odds.py

--- good_start/odds.py
+++ good_start/odds.py
@@ -1,60 +1,33 @@
-import random
 from matplotlib import pyplot as plt
 from collections import Counter
-from utils import BIRD_IMPORTANCE, BIRD_NAMES, BIRD_GROUPS
+from card_selection import *
+from utils import BIRD_NAMES, BIRD_GROUPS
 
 
-def good_game_selection(sample=1000):
-    good_games = Counter()
-    for hand in map(lambda _: random.sample(BIRD_NAMES, 5), range(sample)):
-        for bird in BIRD_IMPORTANCE:
-            required_birds = bird['birds']
-            if all(b in hand for b in required_birds):
-                name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in required_birds)
-                good_games[name] += 1
-    good_games = {k: v / sample for k, v in good_games.items()}
-    plt.bar(good_games.keys(), good_games.values())
-    plt.xticks(rotation = 45)
-    plt.show()
-
-
-def good_game_selection_with_tray(sample=1000):
+def good_game_selection_with_tray(sample:int = 1000):
     good_games = Counter()
     for cards in map(lambda _: random.sample(BIRD_NAMES, 8), range(sample)):
-        hand = cards[:5]
-        tray = cards[5:]
-        for bird in BIRD_IMPORTANCE:
-            required_birds = bird['birds']
-            if all(b in hand for b in required_birds) and any(b in tray for b in bird['tray']):
-                name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in required_birds)
-                good_games[name] += 1
+        hand, tray = cards[:5], cards[5:]
+        if selection := bird_selection(hand, tray):
+            birds, _ = selection
+            name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in birds if b in BIRD_GROUPS)
+            good_games[name] += 1
     good_games = {k: v / sample for k, v in good_games.items()}
     plt.bar(good_games.keys(), good_games.values())
     plt.xticks(rotation=45)
     plt.show()
 
 
-def good_game_selection_with_tray_and_humingbird(sample=1000):
+def good_game_selection_with_tray_and_hummingbird(sample:int =1000):
     good_games = Counter()
-    for cards in map(lambda _: random.sample(BIRD_NAMES, 8), range(sample)):
-        hand = cards[:5]
-        tray = cards[5:]
-        for bird in BIRD_IMPORTANCE:
-            required_birds = bird['birds']
-            required_tray = bird['tray']
-            if all(b in hand for b in required_birds) and \
-                    any(b in BIRD_GROUPS and 'Hummingbird' in BIRD_GROUPS[b] for b in tray) and \
-                any(b in required_tray and not (b in BIRD_GROUPS and 'Hummingbird' in BIRD_GROUPS[b]) for b in tray):
-                name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in required_birds)
-                good_games[name] += 1
+    for cards in map(lambda _: random.sample(BIRD_NAMES, 28), range(sample)):
+        hand, tray, other = cards[:5], cards[5:8], cards[8:]
+        if (selection := bird_selection(hand, tray)) and \
+                        any(b in utils.BIRD_GROUPS and 'Hummingbird' in utils.BIRD_GROUPS[b] for b in other):
+            birds, _ = selection
+            name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in birds if b in BIRD_GROUPS)
+            good_games[name] += 1
     good_games = {k: v / sample for k, v in good_games.items()}
     plt.bar(good_games.keys(), good_games.values())
     plt.xticks(rotation=45)
     plt.show()
-
-
-n = 1000000
-# good_game_selection(n)
-good_game_selection_with_tray(n)
-good_game_selection_with_tray_and_humingbird(n)
-

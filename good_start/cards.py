e366457e6a9d3967c4f20c9b5d2ab678d598aaab
Zac Todd <zactodd0@gmail.com>
2022-05-06 10:34:30 +1200

    Update cards.py

--- good_start/cards.py
+++ good_start/cards.py
@@ -6,21 +6,21 @@ import utils
 
 _BIRD_NAMES = osp.join(utils.RESOURCES, 'bird_names')
 with open(osp.join(_BIRD_NAMES, 'base.txt'), 'r') as f:
-    _BASE = f.read().splitlines()
+    _BASE_BIRDS = f.read().splitlines()
 
 with open(osp.join(_BIRD_NAMES, 'ss.txt'), 'r') as f:
-    _SS = f.read().splitlines()
+    _SS_BIRDS = f.read().splitlines()
 
 with open(osp.join(_BIRD_NAMES, 'ee.txt'), 'r') as f:
-    _EE = f.read().splitlines()
+    _EE_BIRDS = f.read().splitlines()
 
 
-ALL_BIRDS_DECK = _BASE + _SS + _EE
-BASE_DECK = _BASE
+ALL_BIRDS_DECK = _BASE_BIRDS + _SS_BIRDS + _EE_BIRDS
+BASE_DECK = _BASE_BIRDS
 _DECKS = {
-    'base': _BASE,
-    'ss': _SS,
-    'ee': _EE,
+    'base': _BASE_BIRDS,
+    'ss': _SS_BIRDS,
+    'ee': _EE_BIRDS,
 }
 
 

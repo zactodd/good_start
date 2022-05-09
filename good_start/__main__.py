399edf6751a129b5ec56ea6246075f82988f1f4c
Zac Todd <zactodd0@gmail.com>
2022-05-09 13:26:28 +1200

    Update __main__.py

--- good_start/__main__.py
+++ good_start/__main__.py
@@ -67,7 +67,6 @@ if __name__ == '__main__':
             time.sleep(20)
             gi.activate_window()
             gi.menu_from_start()
-        checks += 1
         run_time = time.perf_counter()
         time.sleep(1)
         try:
@@ -75,78 +74,80 @@ if __name__ == '__main__':
             gi.move_and_click(*kp.OVERVIEW_BUTTON)
             time.sleep(2)
             tray = gi.extract_tray_cards()
-            time.sleep(3)
-
-            # Start Turn
-            gi.move_and_click(*kp.TURN_START_BUTTON)
-            time.sleep(1)
-
-            # Read bird cards
-            birds, centres, bird_image = gi.extract_bird_cards()
-            bird_centres = dict(zip(birds, centres))
-
-            # Select birds, food and bonus cards if valid birds in hand
-            if selection := cs.bird_selection(birds, tray):
-                selected_birds, food = selection
-                logging.info(f'Selected birds: {selected_birds}')
-
-                # Select birds
-                time.sleep(0.5)
-                for b in selected_birds:
-                    gi.move_and_click(*bird_centres[b])
-                    time.sleep(0.5)
-
-                # Select food
-                for f in food:
-                    gi.move_and_click(*kp.RESOURCES_TO_BUTTONS[f.lower()])
-                    time.sleep(0.5)
-                gi.move_and_click(*kp.NEXT_BUTTON)
+            if any(b in cards.Deck().possible_tray_birds for b in tray):
+                time.sleep(3)
+                # Start Turn
+                gi.move_and_click(*kp.TURN_START_BUTTON)
                 time.sleep(1)
 
-                # Select bonus cards
-                bonuses, centres, bonus_image = gi.extract_bonus_cards()
-                bonus_centres = dict(zip(bonuses, centres))
-                bonus = cs.bonus_selection(bonuses)
-                gi.move_and_click(*bonus_centres[bonus])
-                time.sleep(0.5)
+                # Read bird cards
+                birds, centres, bird_image = gi.extract_bird_cards()
+                bird_centres = dict(zip(birds, centres))
 
-                gi.move_and_click(*kp.NEXT_BUTTON)
-                if True: #any(b in utils.BIRD_GROUPS and 'hummingbird' in utils.BIRD_GROUPS[b].lower() for b in tray):
-                    time.sleep(3)
-                    # Save game
-                    selected_games.append((birds, selected_birds, food, bonuses, bonus, bird_image, bonus_image))
-                    successes += 1
-                    logging.info(f'Successes: {successes} humming bird in tray.')
-                    gi.new_game_from_game()
-                else:
-                    time.sleep(30)
-                    gi.move_and_click(*kp.TURN_START_BUTTON)
+                # Select birds, food and bonus cards if valid birds in hand
+                if selection := cs.bird_selection(birds, tray):
+                    selected_birds, food = selection
+                    logging.info(f'Selected birds: {selected_birds}')
+
+                    # Select birds
+                    time.sleep(0.5)
+                    for b in selected_birds:
+                        gi.move_and_click(*bird_centres[b])
+                        time.sleep(0.5)
+
+                    # Select food
+                    for f in food:
+                        gi.move_and_click(*kp.RESOURCES_TO_BUTTONS[f.lower()])
+                        time.sleep(0.5)
+                    gi.move_and_click(*kp.NEXT_BUTTON)
                     time.sleep(1)
-                    gi.move_and_click(*kp.OVERVIEW_BUTTON)
-                    time.sleep(1)
-                    gi.select_bird(selected_birds[0], len(selected_birds))
-                    time.sleep(30)
-                    gi.move_and_click(*kp.TURN_START_BUTTON)
+
+                    # Select bonus cards
+                    bonuses, centres, bonus_image = gi.extract_bonus_cards()
+                    bonus_centres = dict(zip(bonuses, centres))
+                    bonus = cs.bonus_selection(bonuses)
+                    gi.move_and_click(*bonus_centres[bonus])
                     time.sleep(0.5)
 
-                    for pos in kp.PLAYER_BOARDS_POSITIONS[1:]:
-                        gi.move_and_click(*pos)
+                    gi.move_and_click(*kp.NEXT_BUTTON)
+                    if True: #any(b in utils.BIRD_GROUPS and 'hummingbird' in utils.BIRD_GROUPS[b].lower() for b in tray):
                         time.sleep(3)
-                        if any(b in utils.BIRD_GROUPS and 'Hummingbird' in utils.BIRD_GROUPS[b]
-                               for h in gi.extract_player_board() for b in h):
-                            selected_games.append((birds, selected_birds, food, bonuses, bonus, bird_image, bonus_image))
-                            successes += 1
-                            logging.info(f'{successes} Successes')
-                            gi.new_game_from_game()
-                            break
-                    else:
-                        logging.info(f'No Hummingbird found in player boards.')
+                        # Save game
+                        selected_games.append((birds, selected_birds, food, bonuses, bonus, bird_image, bonus_image))
+                        successes += 1
+                        logging.info(f'Successes: {successes} humming bird in tray.')
                         gi.new_game_from_game()
+                    else:
+                        time.sleep(30)
+                        gi.move_and_click(*kp.TURN_START_BUTTON)
+                        time.sleep(1)
+                        gi.move_and_click(*kp.OVERVIEW_BUTTON)
+                        time.sleep(1)
+                        gi.select_bird(selected_birds[0], len(selected_birds))
+                        time.sleep(30)
+                        gi.move_and_click(*kp.TURN_START_BUTTON)
+                        time.sleep(0.5)
+
+                        for pos in kp.PLAYER_BOARDS_POSITIONS[1:]:
+                            gi.move_and_click(*pos)
+                            time.sleep(3)
+                            if any(b in utils.BIRD_GROUPS and 'Hummingbird' in utils.BIRD_GROUPS[b]
+                                   for h in gi.extract_player_board() for b in h):
+                                selected_games.append((birds, selected_birds, food, bonuses, bonus, bird_image, bonus_image))
+                                successes += 1
+                                logging.info(f'{successes} Successes')
+                                gi.new_game_from_game()
+                                break
+                        else:
+                            logging.info(f'No Hummingbird found in player boards.')
+                            gi.new_game_from_game()
             else:
+                average_time = (average_time * checks + (time.perf_counter() - run_time)) / (checks + 1)
+                checks += 1
                 gi.new_game_from_game()
         except (SystemError, ValueError) as e:
             traceback.print_exception(*sys.exc_info())
             root_logger.error(e)
             gi.kill_window()
             time.sleep(3)
-        average_time = (average_time * (checks - 1) + (time.perf_counter() - run_time)) / checks
+

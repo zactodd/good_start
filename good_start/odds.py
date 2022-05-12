09abc6601564f9be9d8ad0bba2ac5bf338de367f
Zac Todd <zactodd0@gmail.com>
2022-05-12 19:30:35 +1200

    Update odds.py

--- good_start/odds.py
+++ good_start/odds.py
@@ -1,16 +1,16 @@
 from matplotlib import pyplot as plt
 from collections import Counter
-from typing import List
+import cards
 from card_selection import *
 from utils import BIRD_GROUPS
 
 
 
-def good_game_selection_with_tray(bird_deck: List[str], bird_importance, sample:int = 1000) -> None:
+def good_game_selection_with_tray(sample:int = 1000) -> None:
     good_games = Counter()
-    for cards in map(lambda _: random.sample(bird_deck, 8), range(sample)):
-        hand, tray = cards[:5], cards[5:]
-        if selection := bird_selection(bird_importance, hand, tray):
+    for c in map(lambda _: random.sample(cards.Deck().birds, 8), range(sample)):
+        hand, tray = c[:5], c[5:]
+        if selection := bird_selection(hand, tray):
             birds, _ = selection
             name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in birds if b in BIRD_GROUPS)
             good_games[name] += 1
@@ -21,11 +21,11 @@ def good_game_selection_with_tray(bird_deck: List[str], bird_importance, sample:
     plt.show()
 
 
-def good_game_selection_with_tray_and_hummingbird(bird_deck: List[str], bird_importance, sample:int = 1000) -> None:
+def good_game_selection_with_tray_and_hummingbird(sample:int = 1000) -> None:
     good_games = Counter()
-    for cards in map(lambda _: random.sample(bird_deck, 28), range(sample)):
-        hand, tray, other = cards[:5], cards[5:8], cards[8:]
-        if (selection := bird_selection(bird_importance, hand, tray)) and \
+    for c in map(lambda _: random.sample(cards.Deck().birds, 28), range(sample)):
+        hand, tray, other = c[:5], c[5:8], c[8:]
+        if (selection := bird_selection(hand, tray)) and \
                         any(b in utils.BIRD_GROUPS and 'Hummingbird' in utils.BIRD_GROUPS[b] for b in other):
             birds, _ = selection
             name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in birds if b in BIRD_GROUPS)
@@ -37,12 +37,11 @@ def good_game_selection_with_tray_and_hummingbird(bird_deck: List[str], bird_imp
     plt.show()
 
 
-# from cards import ALL_DECK, build_deck
-# n = 100000
-# birds, _, bird_importance, *_ = ALL_DECK
-# good_game_selection_with_tray(birds, bird_importance, n)
-# good_game_selection_with_tray_and_hummingbird(birds, bird_importance, n)
-#
-# birds, _, bird_importance, *_ = build_deck(['base'])
-# good_game_selection_with_tray(birds, bird_importance, n)
-# good_game_selection_with_tray_and_hummingbird(birds, bird_importance, n)
+n = 100000
+cards.Deck(['base', 'ss', 'ee'])
+good_game_selection_with_tray(n)
+good_game_selection_with_tray_and_hummingbird(n)
+
+cards.Deck(['base'])
+good_game_selection_with_tray(n)
+good_game_selection_with_tray_and_hummingbird(n)

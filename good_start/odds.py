051b4bf514c6d4f56e10ddaf4ac5cb28df707b9a
Zac Todd <zactodd0@gmail.com>
2022-05-28 11:58:09 +1200

    Update odds.py

--- good_start/odds.py
+++ good_start/odds.py
@@ -1,7 +1,8 @@
 from matplotlib import pyplot as plt
+import random
 from collections import Counter
 import cards
-from card_selection import *
+import selection
 from utils import BIRD_GROUPS
 
 
@@ -10,8 +11,8 @@ def good_game_selection_with_tray(sample:int = 1000) -> None:
     good_games = Counter()
     for c in map(lambda _: random.sample(cards.Deck().birds, 8), range(sample)):
         hand, tray = c[:5], c[5:]
-        if selection := bird_selection(hand, tray):
-            birds, _ = selection
+        if selected := selection.bird_selection(hand, tray):
+            birds, _ = selected
             name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in birds if b in BIRD_GROUPS)
             good_games[name] += 1
     good_games = {k: v / sample for k, v in good_games.items()}
@@ -25,9 +26,9 @@ def good_game_selection_with_tray_and_hummingbird(sample:int = 1000) -> None:
     good_games = Counter()
     for c in map(lambda _: random.sample(cards.Deck().birds, 28), range(sample)):
         hand, tray, other = c[:5], c[5:8], c[8:]
-        if (selection := bird_selection(hand, tray)) and \
-                        any(b in utils.BIRD_GROUPS and 'Hummingbird' in utils.BIRD_GROUPS[b] for b in other):
-            birds, _ = selection
+        if (selected := selection.bird_selection(hand, tray)) and \
+                        any(b in BIRD_GROUPS and 'Hummingbird' in BIRD_GROUPS[b] for b in other):
+            birds, _ = selected
             name = '_'.join(''.join(w[0] for w in BIRD_GROUPS[b].split()) for b in birds if b in BIRD_GROUPS)
             good_games[name] += 1
     good_games = {k: v / sample for k, v in good_games.items()}
@@ -37,11 +38,7 @@ def good_game_selection_with_tray_and_hummingbird(sample:int = 1000) -> None:
     plt.show()
 
 
-n = 100000
+n = 10000000
 cards.Deck(['base', 'ss', 'ee'])
-good_game_selection_with_tray(n)
-good_game_selection_with_tray_and_hummingbird(n)
-
-cards.Deck(['base'])
 good_game_selection_with_tray(n)
 good_game_selection_with_tray_and_hummingbird(n)

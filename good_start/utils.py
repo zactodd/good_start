fb60ebec67c0fe3a49443239d979f83fff28174c
Zac Todd <zactodd0@gmail.com>
2025-07-13 21:49:26 +1200

    threadin utils

--- good_start/utils.py
+++ good_start/utils.py
@@ -81,3 +81,36 @@ def thread_wrapper(func: F) -> F:
         return Thread(target=func, args=args, kwargs=kwargs)
     return wrapper
 
+
+def grouper(iterable: Iterable, n: int) -> Iterable:
+    """
+    Groups the iterable into a iterable of iterable of len n,
+    e.g.((x0, x1, ..., xn-1), ((xn, xn+1, ..., x2n-1)), ...)
+    :param iterable: The iterable to be grouped.
+    :param n: The length of the groups. (The last group may be less the n in length.)
+    :return: An iterable which groups objects into batches.
+    """
+    return zip_discard_generator(*([iter(iterable)] * n))
+
+
+def zip_discard_generator(*iterables, sentinel: Any = object()):
+    return ((entry for entry in iterable if entry is not sentinel)
+            for iterable in zip_longest(*iterables, fillvalue=sentinel))
+
+
+
+def parallel_evaluate_iterable(iterable, generate_thread_func: Callable[..., Thread], num_threads: int) -> None:
+    """
+    Evaluates a function over an iterable in parallel over several threads.
+    :param iterable: The items to be evaluated.
+    :param generate_thread_func: The function evaluating the items.
+    :param num_threads: The number of threads to use.
+    """
+    if len(iterable) <= num_threads:
+        threads = map(generate_thread_func, iterable)
+        start_join_threads(threads)
+    else:
+        for g in grouper(iterable, num_threads):
+            threads = map(generate_thread_func, g)
+            start_join_threads(threads)
+
